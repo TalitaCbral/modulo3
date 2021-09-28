59db8c851eeb138b550125dbb9307657a4743f28
Elisama Talita Cabral Santana <82914088+TalitaCbral@users.noreply.github.com>
2021-09-28 20:57:46 -0300

    ok

--- aula-07/index.js
+++ aula-07/index.js
@@ -1,1 +1,16 @@
-console.log("olá");
+// import express
+const express = require('express');
+
+// inicializar express
+const app = express()
+
+// declarando porta
+const port = 3000;
+
+app.get('/', (req, res)=> {
+    res.send('Hello Bluemers')
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`)
+})
