eead52c2acb39a06d194da8c06c48641eaff3494
Rudhy Pereira <rudhy.pereira@gmail.com>
2021-10-02 19:02:12 -0300

    ..

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const FilmesRoutes = require('./routes/filmes.routes');
+
+app.use('/filmes', FilmesRoutes);
+
+
+app.get('/', (req, res)=>{
+    console.info('Seja bem vind@ à minha lista de FIlmes Favoritos!');
+})
+
+
+const port = 3000;
+
+app.listen(port, ()=>{
+    console.info(`O servidor está rodando no endereço: http://localhost:${port}/`);
+})
---
+++ Backend/routes/filmes.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+
+//-------------Exportação do routes-----------------------
+module.exports = router;
