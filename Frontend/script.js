55d398b80902eaabd50379bd0633dd83188f2873
Rudhy Pereira <rudhy.pereira@gmail.com>
2021-10-02 17:44:03 -0300

    re-iniciando o back

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,6 +1,41 @@
-moviesJson.map((item, index)=>{
-    let cardFilm = document.querySelector('.section-card .card-film').cloneNode(true); //clona a section que contem os cards dos filmes
+//Lista com filmes chumbados
+
+let movies = [
+    {
+    id: Date.now(),
+    name: 'Interstellar', 
+    img: './img/Interstellar.gif', 
+    genre: 'Sci-fi', 
+    rate: 10
+    },
+
+    {
+    id: Date.now(), 
+    name: 'Arrival',
+    img: './img/arrival.jpg', 
+    genre: 'Sci-fi', 
+    rate: 8.5
+    },
+    {
+    id: Date.now(), 
+    name: 'Hauru no Ugoku Shiro',
+    img: './img/Hauru no Ugoku Shiro.jpg', 
+    genre: 'Animação', 
+    rate: 9.6
+    },
+];
+
+//------------------------------------------------------------------
+
+
+
+movies.map((item, index)=>{
     
+    let cardFilm = document.querySelector('.card-film').cloneNode(true); //clona a section que contem os cards dos filmes
+    
+    // cardFilm.innerHTML = '';
+    
+
     cardFilm.querySelector('.movieName').innerHTML = item.name;
     cardFilm.querySelector('.igmUrl').src = item.img;
     cardFilm.querySelector('.movieGenre').innerHTML = item.genre;
@@ -9,11 +44,21 @@ moviesJson.map((item, index)=>{
     document.querySelector('.section-card').style.display = 'flex'; //altera o display none para flex para mostrar o card
 
 
-
     document.querySelector('.section-card').append(cardFilm);
     
-    
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
